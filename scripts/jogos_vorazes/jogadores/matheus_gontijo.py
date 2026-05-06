bb264d91052edf5f705510fc1db3ce18041b9265
Luis Eduardo Craizer <lecraizer@gmail.com>
2026-05-05 22:06:40 -0300

    refactoring again and again

--- scripts/jogos_vorazes/jogadores/matheus_gontijo.py
+++
@@ -1,8 +0,0 @@
-def estrategia(vida, num_jogadores_vivos, rodada):
-
-    total_inicial = num_jogadores_vivos
-    if num_jogadores_vivos < total_inicial:
-        return "atacar"
-    else:
-        return "defender"
-    
--- scripts/jogos_vorazes/old_jogadores/kamikaze.py
+++
@@ -1,5 +0,0 @@
-import random
-
-# Função que define a estratégia de Kamikaze
-def estrategia_kamikaze(vida, num_jogadores_vivos, rodada):
-    return "atacar"
